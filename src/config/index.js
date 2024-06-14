61e4e4bdcd9033a7c44e59cf9be1fb1d4f82cb80
ahmed <abenlebna@qualycloud.com>
2024-06-14 16:47:11 +0200

    update config data for env

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,11 @@
-require("dotenv");
+const envFile = require("dotenv");
 
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `./.env.${process.env.NODE_ENV}`;
+  envFile.config({ path: configFile, override: true });
+} else {
+  envFile.config();
+}
 const DB_URL = `mongodb+srv://${process.env.USER_DB}:${process.env.PWD_DB}@datatemplateapi.76megss.mongodb.net/?retryWrites=true&w=majority&appName=dataTemplateApi`;
 
 module.exports = {
