da3721f8e1800e4c9c4ef067311822ba19cc749f
ahmed <abenlebna@qualycloud.com>
2024-06-17 09:58:55 +0200

    add comment

--- src/index.js
+++ src/index.js
@@ -3,26 +3,32 @@ require("dotenv").config();
 const { dbConnection } = require("./database");
 const { PORT } = require("./config");
 
-const startServer = async () => {
+const startApplication = async () => {
   try {
+    // Connect to the database before starting the server
     await dbConnection();
 
+    // Create an express app instance
     const app = express();
 
+    // Middlewares to parse the incoming request data
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    const baseRouter = require("./routes/router");
-    app.use("/", baseRouter);
 
+    // Routes for the application
+    app.use("/", require("./routes/router"));
+
+    // Start the server on the specified port
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.log("**********************");
-    console.log("error", error);
+    console.log("error starting server", error);
     console.log("**********************");
     process.exit(1);
   }
 };
 
-startServer();
+// Start the application by calling the startApplication function
+startApplication();
