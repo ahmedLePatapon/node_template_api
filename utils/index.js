0159b91e44547cb91dc0f5c8989af2582e584aab
ahmed <abenlebna@qualycloud.com>
2024-06-25 09:38:47 +0200

    add module & function to encrypt password

---
+++ utils/index.js
@@ -0,0 +1,17 @@
+const bcrypt = require("bcrypt");
+
+module.exports.GenerateSalt = async () => {
+  return await bcrypt.genSalt();
+};
+
+module.exports.GeneratePassword = async (password, salt) => {
+  return await bcrypt.hash(password, salt);
+};
+
+module.exports.ValidatePassword = async (
+  enteredPassword,
+  savedPassword,
+  salt
+) => {
+  return (await this.GeneratePassword(enteredPassword, salt)) === savedPassword;
+};
